a8f55101ca2573194fddfe93af2ace8149f0d69f
Robs <robertmichaellim@gmail.com>
2017-03-06 00:08:06 +0800

    Added Jumbotron for the header

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <html>
 <head>
 	<link rel = 'stylesheet' href = 'css/bootstrap.min.css'>
+	
 </head>
 <body>
+	<div class = 'container-fluid'>
+		<div class = 'row'>
+			<div class = 'jumbotron'>
+				<h1>Guides</h1>
+				<label>Description here</label>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
