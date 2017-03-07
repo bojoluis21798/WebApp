2880508b40184f874fa5e92a96b9f79923625ed2
bojoluis21798 <trinity21798@gmail.com>
2017-03-07 23:00:08 +0800

    Restructure Directory
    Deleted images that are not gonnna be used. Added login index.php page
    (omg this is so cool and convenient) and resources needed

--- database_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$conn = mysqli_connect("127.0.0.1", "root", "", "cs2205_store");
-	if(!$conn){
-		echo "Error Connecting to the database!";
-		exit();
-	}
-?>
--- index.php
+++ index.php
@@ -1,19 +1,39 @@
-<?php
-	require("database_config.php");
-?>
 <html>
-<head>
-	<link rel = 'stylesheet' href = 'css/bootstrap.min.css'>
-	
-</head>
-<body>
-	<div class = 'container-fluid'>
-		<div class = 'row'>
-			<div class = 'jumbotron'>
-				<h1>Guides</h1>
-				<label>Description here</label>
+	<link rel='stylesheet' href='css/index.css'/>
+	<script src='js/jquery.min.js'></script>
+	<script src='js/index.js'></script>
+	<head>
+		<title>Tactics</title>
+	</head>
+	<body>
+		<div class='menubg'>
+			<div class='container-fluid'>
+				<div class='loginform'>
+					<h1 class='logintitle text-center'>TACTICS</h1>
+					<div class='formproper'>
+						<form>
+							<div class='form-group formrow row'>
+								<label class='h3 text-left col-form-label col-md-11 col-md-offset-1'>Username</label>
+								<div class='col-md-10 col-md-offset-1'>
+									<input type='text' class='form-control' required>
+								</div>
+							</div>
+							<div class='form-group formrow row'>
+								<label class='h3 text-left col-form-label col-md-11 col-md-offset-1'>Password</label>
+								<div class='col-md-10 col-md-offset-1'>
+									<input type='password' class='form-control' required>
+								</div>
+							</div>
+							<div class='row forbut'>
+								<button type='submit' class='btn-lg but1 col-md-10 col-md-offset-1 btn btn-default'>Log In</button>
+							</div>
+							<div class='row'>
+								<button type='submit' class='btn-lg but2 col-md-10 col-md-offset-1 btn btn-default'>Register</button>
+							</div>
+						</form>
+					</div>
+				</div>
 			</div>
 		</div>
-	</div>
-</body>
+	</body>
 </html>
