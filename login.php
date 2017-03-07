f0ff4c649d55f73cb65679c309d8fa1d90740058
Robs <robertmichaellim@gmail.com>
2017-03-08 00:04:13 +0800

    added login

--- login.php
+++ login.php
@@ -1,8 +1,21 @@
 <?php
 	require("conn.php");
-	
+	session_start();
+
 	if(empty($_POST['username']) || empty($_POST['username'])){
 		echo "no username is typed";
 		header("location:index.php");
 	}
+	
+	$sql = "SELECT * FROM users WHERE username = '{$_POST['username']}' and password = '{$_POST['password']}'";
+	$result = mysqli_query($conn, $sql);
+	
+	$count = mysqli_num_rows($result);
+	
+	if($count == 1){
+		echo "login successful!";
+	}else{
+		header("location:index.php");
+	}	
+
 ?>
