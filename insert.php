73ade1cb2e56388d601ff8c870065b044b88225f
Robs <robertmichaellim@gmail.com>
2017-03-09 19:01:24 +0800

    added register.php

--- insert.php
+++ insert.php
@@ -1,11 +1,6 @@
 <?php
 	require("conn.php");
-	if($_POST['pass'] == $_POST['conpass']){
-		$sql = "INSERT into users(username, password) values ('{$_POST['user']}','{$_POST['pass']}')";
-		if($result = mysqli_query($conn,$sql)){
-			header("location:index.php");
-		}
-	}else{
-		header("location:register.php");
-	}
+	$sql = "INSERT into users(username, password) values ('{$_POST['username']}','{$_POST['password']}')";
+	$result = mysqli_query($conn,$sql);
+	echo json_encode("itworks");
 ?>
--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 	<link rel='stylesheet' href='css/bootstrap.css'/>
 	<link rel='stylesheet' href='css/index.css'/>
 	<script src='js/jquery.min.js'></script>
-	<script src='js/index.js'></script>
+	<script src='js/register.js'></script>
 	<head>
 		<title>Register</title>
 	</head>
@@ -12,29 +12,31 @@
 				<div class='loginform'>
 					<h1 class='logintitle text-center'>REGISTER</h1>
 					<div class='formproper'>
-						<form method='POST' action='insert.php'>
+						<div class = 'form'>
 							<div class='form-group formrow row'>
 								<label class='h3 text-left col-form-label col-md-11 col-md-offset-1'>Username</label>
 								<div class='col-md-10 col-md-offset-1'>
-									<input name='user' type='text' class='form-control' required>
+									<input id = 'user' name='user' type='text' class='form-control' required>
 								</div>
 							</div>
 							<div class='form-group formrow row'>
 								<label class='h3 text-left col-form-label col-md-11 col-md-offset-1'>Password</label>
 								<div class='col-md-10 col-md-offset-1'>
-									<input name='pass' type='password' class='form-control' required>
+									<input id = 'pass' name='pass' type='password' class='form-control' required>
+									<span class='passdanger text-danger'></span>
 								</div>
 							</div>
 							<div class='form-group formrow row'>
 								<label class='h3 text-left col-form-label col-md-11 col-md-offset-1'>Confirm Password</label>
 								<div class='col-md-10 col-md-offset-1'>
-									<input name='conpass' type='password' class='form-control' required>
+									<input id = 'conpass' name='conpass' type='password' class='form-control' required>
+									<span class='passdanger text-danger'></span>
 								</div>
 							</div>
 							<div class='row forbut'>
 									<input type='submit' id='reg' class='btn-lg but2 col-md-10 col-md-offset-1 btn btn-default' value = 'Register'>
 							</div>
-						</form>
+						</div>
 					</div>
 				</div>
 			</div>
