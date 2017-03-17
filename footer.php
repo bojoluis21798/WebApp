db111532e327f0f2e08602764bb9fe2d513cc06c
Bubart <robertmichaellim@gmail.com>
2017-03-17 13:02:38 +0800

    added footer.php

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+	require("header.php");
+?>
+<style>
+	a:hover{
+		text-decoration:none;
+	}
+</style>
+<hr>
+<div class = 'container-fluid'>
+	<div class = 'row'>
+		<div class = 'col-md-2'>
+			<p class = 'text-left'>© Bojo and Robert's Work</p>
+		</div>
+
+		<div class = 'col-md-1'>
+			<p class = 'text-left'><a href = '#'>Contact Us</a></p>
+		</div>
+		<div class = 'col-md-2'>
+			<p class = 'text-center'><a href = '#'>Privacy Policy</a></p>
+		</div>
+		<div class = 'col-md-2'>
+			<p class = 'text-left'><a href = '#'>User Agreement</a></p>
+		</div>
+
+	</div>
+</div>
