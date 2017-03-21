442d8ed5a0003ff9a6ebe3406991fe85ab423367
bojoluis21798 <trinity21798@gmail.com>
2017-03-21 08:49:17 +0800

    Added more points for map

--- js/map.js
+++ js/map.js
@@ -1,4 +1,6 @@
 var thread_id = [];
+//index 0 - gif animation
+//index 1 - blinking animation
 
 function gifanim(m_id){	
 	$(".gifanim").attr("src","");
@@ -25,21 +27,21 @@ function gifanim(m_id){
 
 function loadsmoke(){
 	for(var i=0; i<smokes.length; i++){
-		$(".mapprop").append("<div class='point' id='p"+(i+1)+"'></div>");
-		$(".mapprop").append("<div class='target' id='t"+(i+1)+"'><span class='glyphicon glyphicon-remove'></span></div>");
-		$("#p"+(i+1)).css("top",smokes[i].y).css("left",smokes[i].x);
-		$("#t"+(i+1)).css("top",smokes[i].t_y).css("left",smokes[i].t_x);
-		$("#t"+(i+1)).hide();
+		if(id == smokes[i].map_id){
+			$(".mapprop").append("<div class='point' id='p"+(i+1)+"'></div>");
+			$(".mapprop").append("<div class='target' id='t"+(i+1)+"'><span class='glyphicon glyphicon-remove'></span></div>");
+			$("#p"+(i+1)).css("top",smokes[i].y).css("left",smokes[i].x);
+			$("#t"+(i+1)).css("top",smokes[i].t_y).css("left",smokes[i].t_x);
+		}
+		$(".target").hide();
 	}
 	
 }
 
 function blink(selector){
 	thread_id[1] = setInterval(function(){
-		$(selector).fadeOut(200, function(){
-			$(selector).fadeIn(200,function(){
-				$(selector).fadeOut(200);
-			});
+		$(selector).fadeIn(200, function(){
+			$(selector).fadeOut(200);
 		});
 	},200);
 }
@@ -83,22 +85,22 @@ $(document).ready(function(){
 		$(this).children().fadeOut(100);
 	});
 	
-	var map_id;
+	var pid;
 	$(".point").mouseenter(function(){
 		$(this).css("border-color","white");
 		$(".cap").hide();
 		$(".vid").css("border","0.1em solid white");
 		$(".gifanim").show();
 		
-		map_id = $(this).attr("id");
-		$("#t"+map_id[1]).show();
-		blink($("#t"+map_id[1]));
-		gifanim(map_id[1]);
+		pid = $(this).attr("id");
+		$("#t"+pid[1]).show();
+		blink($("#t"+pid[1]));
+		gifanim(pid[1]);
 	});
 	
 	$(".point").mouseleave(function(){
 		clearInterval(thread_id[1]);
-		$("#t"+map_id[1]).hide();
+		$("#t"+pid[1]).hide();
 		$(this).css("border-color","yellow");
 		$(".vid").css("border","0.1em dashed white");
 		$(".cap").show();
