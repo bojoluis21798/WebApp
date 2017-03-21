506fc92bc531ce70c676118e6e7f5cd40e99530b
bojoluis21798 <trinity21798@gmail.com>
2017-03-22 01:21:42 +0800

    Deleted login and register

--- insert.php
+++
@@ -1,6 +0,0 @@
-<?php
-	require("conn.php");
-	$sql = "INSERT into users(username, password) values ('{$_POST['username']}','{$_POST['pass']}')";
-	$result = mysqli_query($conn,$sql);
-	echo json_encode($result);
-?>
